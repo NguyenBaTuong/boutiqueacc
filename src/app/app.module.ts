cc581edf1a3b0652202e5644401b38f477945887
NguyenBaTuong <vansong2204@gmail.com>
2021-07-25 16:45:58 +0700

    home_page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from'./app-routing.module';
+import { HeaderComponent } from './layout/header/header.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
